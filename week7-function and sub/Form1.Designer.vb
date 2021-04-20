3de5c78899633579d0bed763fad6bd472ce68a2f
Nitasha Martin <shukshara09@gmail.com>
2021-04-20 12:05:24 +0800

    Add project files.

---
+++ week7-function and sub/Form1.Designer.vb
@@ -0,0 +1,170 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btn1 = New System.Windows.Forms.Button()
+        Me.lbl1 = New System.Windows.Forms.Label()
+        Me.txtbox1 = New System.Windows.Forms.TextBox()
+        Me.btn2 = New System.Windows.Forms.Button()
+        Me.lbl2 = New System.Windows.Forms.Label()
+        Me.btn3 = New System.Windows.Forms.Button()
+        Me.lbl3 = New System.Windows.Forms.Label()
+        Me.txtbox2 = New System.Windows.Forms.TextBox()
+        Me.btn4 = New System.Windows.Forms.Button()
+        Me.lbl4 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'btn1
+        '
+        Me.btn1.BackColor = System.Drawing.Color.BlanchedAlmond
+        Me.btn1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn1.Location = New System.Drawing.Point(180, 46)
+        Me.btn1.Name = "btn1"
+        Me.btn1.Size = New System.Drawing.Size(83, 43)
+        Me.btn1.TabIndex = 0
+        Me.btn1.Text = "Button1"
+        Me.btn1.UseVisualStyleBackColor = False
+        '
+        'lbl1
+        '
+        Me.lbl1.AutoSize = True
+        Me.lbl1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lbl1.Location = New System.Drawing.Point(511, 52)
+        Me.lbl1.Name = "lbl1"
+        Me.lbl1.Size = New System.Drawing.Size(77, 25)
+        Me.lbl1.TabIndex = 1
+        Me.lbl1.Text = "Label1"
+        '
+        'txtbox1
+        '
+        Me.txtbox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtbox1.Location = New System.Drawing.Point(133, 151)
+        Me.txtbox1.Name = "txtbox1"
+        Me.txtbox1.Size = New System.Drawing.Size(154, 30)
+        Me.txtbox1.TabIndex = 2
+        '
+        'btn2
+        '
+        Me.btn2.BackColor = System.Drawing.Color.BlanchedAlmond
+        Me.btn2.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn2.Location = New System.Drawing.Point(351, 146)
+        Me.btn2.Name = "btn2"
+        Me.btn2.Size = New System.Drawing.Size(88, 44)
+        Me.btn2.TabIndex = 3
+        Me.btn2.Text = "Button1"
+        Me.btn2.UseVisualStyleBackColor = False
+        '
+        'lbl2
+        '
+        Me.lbl2.AutoSize = True
+        Me.lbl2.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lbl2.Location = New System.Drawing.Point(511, 156)
+        Me.lbl2.Name = "lbl2"
+        Me.lbl2.Size = New System.Drawing.Size(77, 25)
+        Me.lbl2.TabIndex = 4
+        Me.lbl2.Text = "Label1"
+        '
+        'btn3
+        '
+        Me.btn3.BackColor = System.Drawing.Color.BlanchedAlmond
+        Me.btn3.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn3.Location = New System.Drawing.Point(180, 237)
+        Me.btn3.Name = "btn3"
+        Me.btn3.Size = New System.Drawing.Size(83, 44)
+        Me.btn3.TabIndex = 5
+        Me.btn3.Text = "Button2"
+        Me.btn3.UseVisualStyleBackColor = False
+        '
+        'lbl3
+        '
+        Me.lbl3.AutoSize = True
+        Me.lbl3.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lbl3.Location = New System.Drawing.Point(511, 245)
+        Me.lbl3.Name = "lbl3"
+        Me.lbl3.Size = New System.Drawing.Size(77, 25)
+        Me.lbl3.TabIndex = 6
+        Me.lbl3.Text = "Label2"
+        '
+        'txtbox2
+        '
+        Me.txtbox2.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtbox2.Location = New System.Drawing.Point(133, 335)
+        Me.txtbox2.Name = "txtbox2"
+        Me.txtbox2.Size = New System.Drawing.Size(154, 30)
+        Me.txtbox2.TabIndex = 7
+        '
+        'btn4
+        '
+        Me.btn4.BackColor = System.Drawing.Color.BlanchedAlmond
+        Me.btn4.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn4.Location = New System.Drawing.Point(351, 330)
+        Me.btn4.Name = "btn4"
+        Me.btn4.Size = New System.Drawing.Size(88, 47)
+        Me.btn4.TabIndex = 8
+        Me.btn4.Text = "Button3"
+        Me.btn4.UseVisualStyleBackColor = False
+        '
+        'lbl4
+        '
+        Me.lbl4.AutoSize = True
+        Me.lbl4.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lbl4.Location = New System.Drawing.Point(511, 338)
+        Me.lbl4.Name = "lbl4"
+        Me.lbl4.Size = New System.Drawing.Size(77, 25)
+        Me.lbl4.TabIndex = 9
+        Me.lbl4.Text = "Label3"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.CadetBlue
+        Me.ClientSize = New System.Drawing.Size(800, 476)
+        Me.Controls.Add(Me.lbl4)
+        Me.Controls.Add(Me.btn4)
+        Me.Controls.Add(Me.txtbox2)
+        Me.Controls.Add(Me.lbl3)
+        Me.Controls.Add(Me.btn3)
+        Me.Controls.Add(Me.lbl2)
+        Me.Controls.Add(Me.btn2)
+        Me.Controls.Add(Me.txtbox1)
+        Me.Controls.Add(Me.lbl1)
+        Me.Controls.Add(Me.btn1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btn1 As Button
+    Friend WithEvents lbl1 As Label
+    Friend WithEvents txtbox1 As TextBox
+    Friend WithEvents btn2 As Button
+    Friend WithEvents lbl2 As Label
+    Friend WithEvents btn3 As Button
+    Friend WithEvents lbl3 As Label
+    Friend WithEvents txtbox2 As TextBox
+    Friend WithEvents btn4 As Button
+    Friend WithEvents lbl4 As Label
+End Class
---
+++ week7-function and sub/Form1.vb
@@ -0,0 +1,5 @@
+﻿Public Class Form1
+    Private Sub btn1_Click(sender As Object, e As EventArgs) Handles btn1.Click
+        lbl1.Text = "My Name Is"
+    End Sub
+End Class
